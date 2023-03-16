9712f58d2c73c2496a1dfafa081424eaed45a474
Nishad <126847444+Nishad0123@users.noreply.github.com>
2023-03-16 10:58:25 +0530

    Add files via upload

---
+++ tick.h
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<list>
+//#include
+
+
+using namespace std;
+
+
+int Xflights=5;
+int Xseats=10;
+
+class User
+{
+};
+
+class Flight
+{
+};
+
---
+++ ticket.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<list>
+
+using namespace std;
+
+class User
+{
+	int usrid;		//user ID
+	string usrname;		//user name
+	string ppno;		//passport no.
+	string flt_no;		//flight no.
+	int tkt_no;		//ticket no.
+
+};
+
+class Flight
+{
+	string flt_no;		//flight no.
+	int timea;		//arrival time
+	int timed;		//departure time
+	string src;		//source
+	string dest;		//destination
+	string amt;		//amount
+	string if_avl=0;	//if flight available
+};
+
+
+int main()
+{
+
+	return 0
+}
